e0be97888f147b436486bd035a81283e96468c85
Furkan Kaynak <furkankaynak.74@gmail.com>
2023-11-26 14:32:20 +0300

    init project with vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
